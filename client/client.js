aff525329f7f011e38035f8ef24c414c69eb42c6
jayesh-ops <jayesh.kanojiya8149@gmail.com>
2024-08-14 11:00:21 +0530

    new commit

--- client/client.js
+++ client/client.js
@@ -1,23 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
-// #region constants
-
-// #endregion
-
-// #region styled-components
-
-// #endregion
-
-// #region functions
-
-// #endregion
-
-// #region component
-const propTypes = {};
-
-const defaultProps = {};
 
 /**
  * 
@@ -26,8 +8,6 @@ const client = () => {
     return <div></div>;
 }
 
-client.propTypes = propTypes;
-client.defaultProps = defaultProps;
-// #endregion
+
 
 export default client;
